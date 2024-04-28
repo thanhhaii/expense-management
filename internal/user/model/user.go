3b59acb43253c8ecaf3d6dd2dbacff6ebbb3f2c7
thanhhaii <hailamnguyenthanh@gmail.com>
2024-04-28 15:36:03 +0700

    fix(user-service): add index column email

--- internal/user/model/user.go
+++ internal/user/model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	Id        int        `json:"id" gorm:"column:id;"`
-	Email     string     `json:"email" gorm:"column:email;"`
+	Email     string     `json:"email" gorm:"column:email;index:idx_email,unique"`
 	Password  string     `json:"password" gorm:"column:password;"`
 	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at;"`
 	UpdatedAt *time.Time `json:"updated_at" gorm:"column:updated_at;"`
