8e0a7810af2948908fabe4f862b0d9df0c13b004
thanhhaii <hailamnguyenthanh@gmail.com>
2024-05-03 23:18:02 +0700

    feat(transaction): create basic module transaction

--- initializers/migrate_database.go
+++ initializers/migrate_database.go
@@ -1,11 +1,12 @@
 package initializers
 
 import (
+	transactionmodel "basicproject/internal/module/transaction/model"
 	"basicproject/internal/module/user/model"
 )
 
 func AutoSyncDatabase() {
-	err := DB.AutoMigrate(&usermodel.User{})
+	err := DB.AutoMigrate(&usermodel.User{}, &transactionmodel.Transaction{})
 	if err != nil {
 		panic("Failed when migrate table")
 	}
